c40b6302f3c84e82c5075a9f22f4001bb197f8ed
taton2 <taton2@naver.com>
2021-04-26 15:27:31 +0900

    #16

--- CSBasic2/Program.cs
+++ CSBasic2/Program.cs
@@ -6,7 +6,50 @@ namespace CSBasic2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("숫자 입력: ");
+            int input = int.Parse(Console.ReadLine());
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("짝수");
+            }
+            if (input % 2 == 1)
+            {
+                Console.WriteLine("홀수");
+            }
+
+            //시각 구하기
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+            Console.WriteLine(DateTime.Now.Millisecond);
+
+            if (DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("오전 입니다");
+            }
+            if (DateTime.Now.Hour == 12)
+            {
+                Console.WriteLine("밥타임 입니다");
+            }
+            if (DateTime.Now.Hour > 12)
+            {
+                Console.WriteLine("오후 입니다");
+            }
+
+            if (DateTime.Now.Hour < 11)
+            {
+                Console.WriteLine("아침 먹을 시간!");
+            }else if (DateTime.Now.Hour < 15)
+            {
+                Console.WriteLine("점심 먹을 시간!");
+            }
+            else
+            {
+                Console.WriteLine("저녁 먹을 시간!");
+            }
         }
     }
 }
