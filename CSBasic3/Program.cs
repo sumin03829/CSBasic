bd83c6cb7575b9abb030b938a5b123fbb9ee68e5
taton2 <taton2@naver.com>
2021-05-21 10:01:17 +0900

    #21 반복문 응용

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSBasic3
@@ -53,9 +54,89 @@ namespace CSBasic3
                 Console.WriteLine((char)i2);
             }*/
 
-            for(i = intArray3.Length - 1; i >= 0; i--)
+            for (i = intArray3.Length - 1; i >= 0; i--)
             {
                 Console.WriteLine(intArray3[i]);
+            }
+
+            string[] array = { "사과", "배", "포도", "딸기", "바나나" };
+            foreach (string item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            for (i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            for (i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j--)
+                    Console.Write(' ');
+                for (int j = 0; j < i + 1; j++)
+                    Console.Write('*');
+                Console.WriteLine("");
+            }
+
+            string inputString = "potato Tomato";
+            inputString.ToUpper();//return POTATO TOMATO
+            Console.WriteLine(inputString);//potato Tomato
+            Console.WriteLine(inputString.ToLower());//potato tomato
+            Console.WriteLine(inputString);//potato Tomato
+
+            string foodString = "감자 고구마 토마토 치킨";
+            string[] foods = foodString.Split(new char[] { ' ' });
+            foreach (var food in foods)
+            {
+                Console.WriteLine(food);
+            }
+
+            string pathString = "C:\\oraclexe\\app\\oracle\\product\\11.2.0\\server\\bin;;C:\\oraclexe\\app\\oracle\\product\\11.2.0\\server\\bin;C:\\Program Files (x86)\\Common Files\\Oracle\\Java\\javapath;C:\\Program Files (x86)\\Intel\\iCLS Client\\;C:\\Program Files\\Intel\\iCLS Client\\;%SystemRoot%\\system32;%SystemRoot%;%SystemRoot%\\System32\\Wbem;%SYSTEMROOT%\\System32\\WindowsPowerShell\\v1.0\\;%SYSTEMROOT%\\System32\\OpenSSH\\;C:\\Program Files (x86)\\Intel\\Intel(R) Management Engine Components\\DAL;C:\\Program Files\\Intel\\Intel(R) Management Engine Components\\DAL;C:\\Program Files (x86)\\Intel\\Intel(R) Management Engine Components\\IPT;C:\\Program Files\\Intel\\Intel(R) Management Engine Components\\IPT;C:\\Program Files\\Java\\jdk1.8.0_201\\bin;C:\\Users\\82104\\Desktop\\java;D:\\eclipse\\chap_05\\src\\chap_05;D:\\eclipse;D:\\eclipse\\chap_05;C:\\Program Files\\Git\\cmd;C:\\python프로젝트\\venv\\Scripts;C:\\Program Files\\nodejs\\;C:\\Program Files\\dotnet\\;C:\\Program Files\\Microsoft SQL Server\\130\\Tools\\Binn\\;C:\\Program Files\\Microsoft SQL Server\\Client SDK\\ODBC\\170\\Tools\\Binn\\;C:\\Program Files\\Microsoft VS Code\\bin;C:\\Program Files (x86)\\dotnet\\";
+            string[] paths = pathString.Split(new char[] { ';' });
+            foreach (var path in paths)
+                Console.WriteLine(path);
+
+            string StrangeInput = " test \n     \n ";
+            Console.WriteLine("[" + StrangeInput + "]");
+            Console.WriteLine("[" + StrangeInput.Trim() + "]");
+
+            string[] foodsArray = { "감자", "고구마", "토마토" };
+            Console.WriteLine(string.Join(",", foodsArray));
+
+            Console.Clear();
+            Console.WriteLine("포맷중: 0% [__________]");
+            var hash = "";
+            for(int f = 0; f < 10; f++)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("포맷중: ");
+                Console.Write((f+1)*10+"% [__________]");
+                for(int f2 = 0; f2 < f; f2++)
+                    Console.Write("#");
+                for (int f2 = f+1; f2 < 10; f2++)
+                    Console.Write("_");
+                Console.WriteLine("]");
+                Thread.Sleep(1000);
+            }
+            Thread.Sleep(3000);
+
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+                if (x % 3 == 0)
+                    Console.WriteLine("___@");
+                else if (x % 3 == 1)
+                    Console.WriteLine("_^@");
+                else
+                    Console.WriteLine("^-@");
+                Thread.Sleep(1000);
+                x++;
             }
         }
     }
