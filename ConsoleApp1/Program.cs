8d46cce4b215d7135403b3586fc0704807a701ab
taton2 <taton2@naver.com>
2021-05-28 09:53:39 +0900

    #26 클래스 응용

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,29 +12,31 @@ namespace CBasic4
         public static double PI = 3.141592;
         public static void Hello()
         {
+            // a = 3; 접근 안됨
             Console.WriteLine("Greeting~");
         }
     }
     class Product
     {
-        public string name = "default";
-        public int price = 100;
+        public string name = "defalut";
+        public int price = 1000;
 
         public override string ToString()
         {
-            return this.name + "/" + this.price;
+            return this.name + " / " + this.price;
         }
     }
     class Car
     {
-        int charNumber;
+        // C# 기본세팅은 private
+        int carNumber;
         DateTime inTime;
         DateTime outTime;
-            
-            public void SetInTime()
+        public void SetInTime()
         {
             this.inTime = DateTime.Now;
         }
+
         public void SetOutTime()
         {
             this.outTime = DateTime.Now;
@@ -45,65 +47,96 @@ namespace CBasic4
         static void Main(string[] args)
         {
             Car[] cars = new Car[10];
-
             Random random = new Random();
             Console.WriteLine(random.Next());
-            Console.WriteLine(random.Next(100));
-            Console.WriteLine(random.Next(10,100));
+            Console.WriteLine(random.Next(100));        // 0 ~ 99 
+            Console.WriteLine(random.Next(10, 000));
 
             Console.WriteLine(random.NextDouble());
             Console.WriteLine(random.NextDouble());
-            Console.WriteLine(random.NextDouble());
-            Console.WriteLine(random.NextDouble()*100);
+            Console.WriteLine(random.NextDouble() * 100);
 
             List<int> list = new List<int>();
             list.Add(52);
-            list.Add(273);
-            list.Add(165);
-            list.Add(64);
+            list.Add(245);
+            list.Add(123);
+            list.Add(56);
             foreach (var item in list)
             {
-                Console.WriteLine("Count:" + list.Count + "\titem:" + item);
+                Console.WriteLine("Count : " + list.Count + "\titme : " + item);
+                // list.Remove(item);   list가 바뀌지 않을 가정하에 foreach문을 돌린다. 
             }
             list.Remove(52);
             foreach (var item in list)
             {
-                Console.WriteLine("Count:" + list.Count + "\titem:" + item);
-                //list.Remove(item);
+                Console.WriteLine("Count : " + list.Count + "\titme : " + item);
             }
 
-            Console.WriteLine(Math.Abs(-52273));
-            Console.WriteLine(Math.Ceiling(52.273));
-            Console.WriteLine(Math.Floor(52.273));
-            Console.WriteLine(Math.Min(52, 273));
-            Console.WriteLine(Math.Max(52, 273));
+            Console.WriteLine(Math.Abs(-2345));
+            Console.WriteLine(Math.Ceiling(89.343));
+            Console.WriteLine(Math.Floor(89.343));
+            Console.WriteLine(Math.Max(45, 78));
+            Console.WriteLine(Math.Min(45, 78));
+            Console.WriteLine(Math.Round(45.78));
 
             Product product = new Product();
             Console.WriteLine(product);
             Product productA = new Product() { name = "감자", price = 2000 };
-            Console.WriteLine(productA);
+            Console.WriteLine(productA.ToString());
             Product productB = new Product() { name = "고구마", price = 3000 };
-            Console.WriteLine(productB);
+            Console.WriteLine(productB.ToString());        // product라고 만 나옴.
 
             Console.WriteLine(MyMath.PI);
             MyMath.Hello();
 
-            List<Student> students = new List<Student>()
-            {
-                new Student() { name = "유인성", grade = 1 },
-                new Student() { name = "연하진", grade = 2 },
-                new Student() { name = "윤명월", grade = 4 },
-                new Student() { name = "구지현", grade = 1 },
-                new Student() { name = "김연화", grade = 2 },
-                new Student() { name = "함기훈", favorityFood = "밀크티" }
-            };
-            /*students.Add(new Student() { name = "유인성", grade = 1 });
+            /*
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { name = "윤인성", grade = 1 });
             students.Add(new Student() { name = "연하진", grade = 2 });
-            students.Add(new Student() { name = "윤아린", grade = 3 });
-            students.Add(new Student() { name = "윤명월", grade = 4 });
-            students.Add(new Student() { name = "구지현", grade = 1 });
-            students.Add(new Student() { name = "김연화", grade = 2 });
-            students.Add(new Student() { name = "함기훈",favorityFood="밀크티"});*/
+            students.Add(new Student() { name = "윤아린", grade = 4 });
+            students.Add(new Student() { name = "사람", grade = 1 });
+            students.Add(new Student() { name = "기본", grade = 2 });
+            students.Add(new Student() { name = "김솔민", favorityFood="치킨" });
+            */
+            List<Student> students = new List<Student>() {
+                new Student() { name = "윤인성", grade = 1 },
+                new Student() { name = "연하진", grade = 2 },
+                new Student() { name = "윤아린", grade = 4 },
+                new Student() { name = "사람", grade = 1 },
+                new Student() { name = "기본", grade = 2 },
+                new Student() { name = "김솔민", favorityFood = "치킨" },
+            };
+
+            foreach (var item in students)
+            {
+                Console.WriteLine(item.name + ":" + item.grade);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (students[i].grade > 0)
+                {
+                    students.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            for(int i = students.Count - 1; i >= 0; i--)
+            {
+                if (students[i].grade > 0)
+                {
+                    students.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            for (int i = students.Count - 1; i >= 0; i--)
+                if (students[i].grade > 0)
+                    students.RemoveAt(i);
+
+                        foreach (var item in students)
+                    Console.WriteLine(item.name + ":" + item.grade);
+            }
         }
     }
 
