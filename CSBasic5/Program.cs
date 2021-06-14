35e9f90445b59541f64adf1fcb06b90f0193510d
taton2 <taton2@naver.com>
2021-06-14 15:30:10 +0900

    #29 오버로딩 접근게한자, 생성자, 소멸자

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -15,22 +15,25 @@ namespace CSBasic5
             Console.WriteLine(ml.Multi(103,32));
             ml.Print();
             Console.WriteLine(ml.Sum(1, 100));
-            Console.WriteLine(MyMath.Abs(52));
-            Console.WriteLine(MyMath.Abs(273));
+            MyMath.Abs(52);
+            MyMath.Abs(273);
+            MyMath.Abs(52.273);
+            MyMath.Abs(3000000000);
         }
 
         class MyMath
         {
-            public static int Abs(int input)
+            public static void Abs(int input)
             {
-                if (input < 0)
-                {
-                    return -input;
-                }
-                else
-                {
-                    return input;
-                }
+                Console.WriteLine("정수: "+ ((input < 0) ? -input : input));  
+            }
+            public static void Abs(double input)
+            {
+                Console.WriteLine("더블: " + ((input < 0) ? -input : input));
+            }
+            public static void Abs(long input)
+            {
+                Console.WriteLine("롱: " + ((input < 0) ? -input : input));
             }
         }
 
