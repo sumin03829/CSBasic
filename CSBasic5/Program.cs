8af9e47c418d8abb7bea473ab4e80ea275af5379
taton2 <taton2@naver.com>
2021-06-14 16:07:20 +0900

    #29 생성자

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -23,6 +23,50 @@ namespace CSBasic5
             ml.somePublic();
             /*ml.someDefault();
             ml.somePrivate();*/
+
+            Product productA = new Product("감자", 2000);
+            Product productB = new Product("고구마", 3000);
+            Console.WriteLine(productA);
+            Console.WriteLine(productB);
+            Console.WriteLine(Product.counter + "개 생성되었습니다.");
+
+            Console.WriteLine("첫 번째 위치");
+            Sample sample = new Sample();
+            // Console.WriteLine(Sample.value);
+            Console.WriteLine("두 번째 위치");
+            Console.WriteLine(Sample.value);
+            //Sample sample = new Sample();
+            Console.WriteLine("세 번째 위치");
+        }
+
+        class Sample
+        {
+            public static int value;
+            static Sample()
+            {
+                value = 10;
+                Console.WriteLine("정적 생성자 호출");
+            }
+        }
+
+        class Product
+        {
+            public static int counter = 0;
+            public int id;
+            public string name;
+            public int price;
+
+            public Product(string name, int price)
+            {
+                Product.counter = Product.counter + 1;
+                this.id = Product.counter;
+                this.name = name;
+                this.price = price;
+            }
+            public override string ToString()
+            {
+                return id + ":" + name;
+            }
         }
 
         class MyMath
